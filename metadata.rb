e7080cdb1a7baab454bb7e4d94a0fa5974d6085f
Eric Heydrick <eheydrick@gmail.com>
2018-04-13 11:44:01 -0700

    Switch back to nginx and update for Chef 13

--- metadata.rb
+++ metadata.rb
@@ -4,12 +4,12 @@ maintainer_email "kallan@riotgames.com"
 license          "Apache 2.0"
 description      "Installs/Configures nexus"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "3.3.2"
+version          "3.3.3"
 
 %w{ ubuntu centos }.each do |os|
   supports os
 end
 
 depends "java", ">= 1.15.4"
-depends "chef_nginx", ">= 2.9.0"
+depends "nginx", ">= 8.1.2"
 depends "artifact", ">= 1.11.0"
